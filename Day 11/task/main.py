b9e69095ac8c001bf4bad37c07163466c0e94edd
Venky <itvenky@gmail.com>
2025-07-15 19:31:24 +0530

    Day 11 completed

---
+++ Day 11/task/main.py
@@ -0,0 +1,64 @@
+import random
+
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
+
+def compare(u_score, c_score):
+    if u_score == c_score:
+        return "Draw"
+    elif c_score == 0:
+        return "Loss, opponent has Blackjack"
+    elif u_score == 0:
+        return "Win with a Blackjack"
+    elif u_score > 21:
+        return "You went over, You lose"
+    elif c_score > 21:
+        return "Opponent went over, You lose"
+    elif u_score > c_score:
+        return "You win"
+    else:
+        return "You lose"
+
+user_cards = []
+computer_cards = []
+is_game_over = False
+computer_score = -1
+user_score = -1
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append((deal_card()))
+
+while not is_game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+
+    print(f"Your cards: {user_cards}, current score is {user_score}")
+    print(f"Computer's first card : {computer_cards[0]}")
+
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        is_game_over = True
+    else:
+        user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+        if user_should_deal == 'y':
+            user_cards.append(deal_card())
+        else:
+            is_game_over = True
+
+while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
+
+compare(compare(user_score,computer_score))

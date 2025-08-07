0d0cc715dc5e155e3772f541d896f49edfc24b35
Venky <itvenky@gmail.com>
2025-08-07 10:55:02 +0530

    Day 13 Completed

--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,8 @@
-age = int(input("How old are you?"))
+try:
+    age = int(input("How old are you?"))
+except ValueError:
+    print("You have typed in a an invalid number.")
+    age = int(input("How old are you?"))
+
 if age > 18:
-print("You can drive at age {age}.")
+    print(f"You can drive at age {age}.")
--- Day 13/Use Print/task.py
+++ Day 13/Use Print/task.py
@@ -1,5 +1,8 @@
 word_per_page = 0
 pages = int(input("Number of pages: "))
-word_per_page == int(input("Number of words per page: "))
+word_per_page = int(input("Number of words per page: "))
 total_words = pages * word_per_page
+
+print(f"pages = {pages}")
+print(f"words per page = {word_per_page}")
 print(total_words)

cc90116de39a8b327701d2ef865f160bd3a0d53f
Venky <itvenky@gmail.com>
2025-08-22 05:44:03 +0530

    Day 14 Completed

---
+++ Day 14/Higher or Lower Project/main.py
@@ -0,0 +1,50 @@
+from art import logo,vs
+from game_data import data
+import random
+
+def format_data(account):
+    # formate the account data into printable format.
+    account_name = account_a["name"]
+    account_descr = account_a["description"]
+    account_country = account_a["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
+
+def check_answer(user_guess, a_followers, b_followers):
+    if a_followers > b_followers:
+        return user_guess == "a"
+    else:
+        return user_guess == "b"
+
+print(logo)
+score = 0
+game_should_continue = True
+account_b = random.choice(data)
+
+while game_should_continue:
+    #Generate a random account from the game data
+    account_a = account_b
+    account_b = random.choice(data)
+    if account_a == account_b:
+        account_b = random.choice(data)
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Compare B: {format_data(account_b)}.")
+
+    #Ask user for a guess
+    guess = input("Who has more followers? Type A or B: ").lower()
+
+    print("\n" * 20)
+    print(logo)
+
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    if is_correct:
+        score += 1
+        print(f"You're right! Current score: {score}")
+    else:
+        print(f"Sorry, that's wrong, Your final score: {score}")
+        game_should_continue = False
